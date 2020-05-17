349ce31820a41cf05807fbe230fd09181c1aba0b
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2020-05-17 14:09:36 +0530

    disply error feature
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- app.js
+++ app.js
@@ -26,7 +26,24 @@ function calculateResulsts(e){
         totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
     }
     else{
-        console.log('please check the values');
+        showerror('Please check your input');
     }
     e.preventDefault();
+}
+
+function showerror(error){
+    const errorDiv = document.createElement('div');
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+
+    card.insertBefore(errorDiv, heading);
+
+    setTimeout(clearError, 2000);
+}
+
+function clearError(){
+    document.querySelector('.alert').remove();
 }
